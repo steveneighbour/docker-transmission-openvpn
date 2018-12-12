2903d14a1a1e61d243320ba94e22869d1e4da1e3
steveneighbour <steve.neighbour@gmail.com>
2018-12-12 11:57:45 +1000

    Update start.sh

--- transmission/start.sh
+++ transmission/start.sh
@@ -76,6 +76,19 @@ fi
 
 if [[ "true" = "$LOG_TO_STDOUT" ]]; then
   LOGFILE=/dev/stdout
+echo "STARTING TRANSMISSION"
+exec su --preserve-environment ${RUN_AS} -s /bin/bash -c "/usr/bin/transmission-daemon -g ${TRANSMISSION_HOME} --logfile ${TRANSMISSION_HOME}/transmission.log" &
+echo "STARTING NZBGET"
+exec su --preserve-environment ${RUN_AS} -s /bin/bash -c "/app/nzbget/nzbget -s -c /config/nzbget.conf -o OutputMode=log" &
+
+if [ "$OPENVPN_PROVIDER" = "PIA" ]
+then
+    echo "CONFIGURING PORT FORWARDING"
+    exec /etc/transmission/updatePort.sh &
+elif [ "$OPENVPN_PROVIDER" = "PERFECTPRIVACY" ]
+then
+    echo "CONFIGURING PORT FORWARDING"
+    exec /etc/transmission/updatePPPort.sh ${TRANSMISSION_BIND_ADDRESS_IPV4} &
 else
   LOGFILE=${TRANSMISSION_HOME}/transmission.log
 fi
